d2492457674a0f05de22af1bd46735ea59c13b39
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-02-19 03:14:54 -0500

    npm

--- distr/js/index.js
+++ distr/js/index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params : {
+			apikey : '6eddeff',
+			s      : 'avengers'
+		}
+	});
+
+	console.log(response.data);
+};
+
+fetchData();
